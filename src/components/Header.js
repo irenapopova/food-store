f955c0aa16f7f2bb5ac5e0fe1f2459fda0401bbb
Irena <irenejpopova6@gmail.com>
2020-06-09 15:42:47 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>Catch of the Day</h1>
+const Header = props => {
 
-        <h3 className="tagline">
-          <span>Fresh Daily</span>
-        </h3>
-      </header>
+  <header className="top">
+    <h1>
+      Catch
+        <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+          Day
+        </h1>
 
-    )
-  }
-}
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+
+};
+
+
+
+Header.propTypes = {
+  tagline: PropTypes.string.isRequired
+};
+
 
 export default Header;
