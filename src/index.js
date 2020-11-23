c7d32dacc5a2ca0421618d687f9f64f7ffbf73aa
Irena <irenejpopova6@gmail.com>
2020-11-23 13:41:06 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./css/style.css";
 render(<StorePicker />, document.querySelector("#main"));
 
 
+// ! every single class in react needs at least one method inside of it, and that method is called render, render determines what html I put on the page, or what DOM element at the end of the day do I render out to the page, and from render I return
 
 // the render method takes two things: first some jsx which is sort of like html 
 // second:  render method needs a mounting point, i need to give an actual DOM element in order to mount to the page. 
